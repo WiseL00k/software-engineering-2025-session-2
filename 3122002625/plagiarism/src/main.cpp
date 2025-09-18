1f5725db55b54908093c56c979d611e93bc8a0b4
WiseLook <1656438881@qq.com>
2025-09-18 22:27:44 +0800

    feat: add input and output path var.

--- 3122002625/plagiarism/src/main.cpp
+++ 3122002625/plagiarism/src/main.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        cerr << "Usage: " << argv[0] 
+             << " <original_file> <plagiarized_file> <output_file>" << endl;
+        return 1;
+    }
+
+    string originalPath = argv[1];
+    string plagiarizedPath = argv[2];
+    string outputPath = argv[3];
+
 
     return 0;
 }
