720053a41d9498355b020feb9c874c5289c3a1ca
WiseLook <1656438881@qq.com>
2025-09-22 23:25:48 +0800

    style: update note.

--- 3122002625/plagiarism/include/Similarity.h
+++ 3122002625/plagiarism/include/Similarity.h
@@ -5,11 +5,21 @@
 
 class Similarity {
 public:
-    // 计算两个字符串的重复率
+    /**
+     * @brief 计算两个文本的重复率（相似度）
+     * @param text1 原文
+     * @param text2 抄袭版
+     * @return 相似度 [0,1]，保留小数两位
+     */
     static double calculate(const std::string &text1, const std::string &text2);
 
 private:
-    // 求两个字符串的最长公共子序列长度
+    /**
+     * @brief 计算最长公共子序列 (LCS) 的长度
+     * @param s1 字符串1
+     * @param s2 字符串2
+     * @return LCS 长度
+     */
     static int lcsLength(const std::string &a, const std::string &b);
 };
 
